94d7defa8abeafaf6e0b9d324c9d68a3a9d1cd42
Gavin Kelly <gavin.kelly@crick.ac.uk>
2023-07-05 13:39:09 +0100

    First version of the cytokine visualisation
    Set missing cytokines to one-third of the lowest value achieved across
    subjects for that cytokine.

---
+++ code/quarto-vis/R/markdown.r
@@ -0,0 +1,110 @@
+##' Make captioning function that generates hyperlinks
+##'
+##' captioner returns a function that can be used in `fig.cap`.  Call
+##' the resulting function with a string containing the caption text,
+##' after any plot, to store a link to the pdf version of the
+##' plot. Call the function without any arguments (e.g. in the
+##' `fig.cap` argument of a chunk) to invoke the captioining mechanism
+##' in the markdown.
+##' @title Caption hyperlinking
+##' @return
+##' @author Gavin Kelly
+#' @export
+captioner <- function() {
+  local({
+    captions <- character()
+    function(caption) {
+      if (missing(caption)) {
+        ret <- captions
+        captions <<- character()
+        return(ret)
+      } else {
+        if (isTRUE(getOption('knitr.in.progress'))) {
+          link_caption <- paste0("[", caption, "](", knitr::fig_path("pdf", number=length(captions)+1), ")")
+        } else {
+          link_caption <- caption
+        }
+        captions <<- c(captions, link_caption)
+      }
+    }
+  })
+}
+
+##' Link GT tables to a csv file
+##'
+##' To be used in a `GT` pipeline, it will store the underlying table
+##' data in a csv file under the given name, and insert a link in the
+##' table's caption that points to the csv file.
+##' @title Link GT tables to a csv file
+##' @param data The GT object with a caption set
+##' @param name The filename of the csv
+##' @return Th GT object (invisibly)
+##' @author Gavin Kelly
+#' @export
+tab_link_caption <- function(data,name) {
+  if (missing(name)) {
+    heading <- gt:::dt_heading_get(data)
+    name <- paste(heading$title, heading$subtitle)
+  }
+  caption <- gt:::dt_options_get_value(data = data, option = "table_caption")
+  fname <- knitr::fig_path("csv", number=name)
+  if (!file.exists(dirname(fname))) dir.create(dirname(fname), recursive=TRUE)
+  write.csv(gt:::dt_data_get(data), file=fname)
+  data <- gt:::dt_options_set_value(
+    data,
+    "table_caption",
+    paste0("[", caption, "](", fname, ")")
+  )
+  invisible(data)
+}
+
+
+##' Generate multiple captions per chunk
+##'
+##' To be used in a `GT` pipeline. Before the call to `gt` to
+##' suffix the chunk label to make the caption unique, and once after
+##' to reset the chunk label to its default
+##' 
+##' @title Multiple GT tables per chunk
+##' @param data The GT object
+##' @param label The text that uniquely identifies this table amongst others in the chunk
+##' @return Th GT object (invisibly)
+##' @author Gavin Kelly
+#' @export
+bookdown_label <- function(data, label="") {
+  current <- knitr::opts_current$get('label')
+  if ("chunk" %in% names(attributes(data))) {
+    knitr::opts_current$set(label=attr(data, "chunk"))
+  } else {
+    attr(data, "chunk") <- current
+    knitr::opts_current$set(label=paste(current, label, sep="-"))
+  }
+  invisible(data)
+}
+
+
+make_param <- function(var) {
+  val <- system(paste0("make print-", var), intern=TRUE, ignore.stderr=TRUE)
+  strsplit(val, "=", fixed=TRUE)[[1]][2]
+  }
+
+
+load_params <- function(prefix, extra=FALSE) {
+    script_params <- knitr::knit_params(readLines(paste0(prefix,".qmd"))) %>%
+      {setNames(lapply(., "[[", "value"), sapply(., "[[", "name"))}
+    if (extra) {
+      if (file.exists(fname <- paste0(prefix, ".yml"))) {
+        yml_params <- yaml::read_yaml(fname)[[1]]$params
+      } else {
+        yml_params <- list()
+      }
+      if (file.exists(fname <- file.path(dirname(prefix), "params.yml"))) {
+        global_params <- yaml::read_yaml(fname)
+      } else {
+       global_params <- list()
+      }
+      Reduce(modifyList, list(global_params, script_params, yml_params))
+    } else {
+      script_params
+      }
+  }
---
+++ code/quarto-vis/SDY180.mk
@@ -0,0 +1,42 @@
+################################################################
+## `param_names` is a list of variable names whose values will
+## be inserted into every script yaml header, over-riding
+## values of the same name from the resources/'section'.qmd file,
+## which in turn over-rides the values in resources/project.yml.
+## Subsection-specific values in resources/'section'.yml
+## will take precedence over any of the other three sources.
+################################################################
+
+param_names=res_dir VERSION TAG staging_dir
+
+
+################################################################
+## `sections` will get used to pick up script names in given 
+## order, so name everything in the 'source-dir' 
+## (`./resources` by default) after one one of these
+################################################################
+
+sections=Heatmap
+
+
+################################################################
+## SLURM
+## 
+## Have default but customisable slurm parameters 
+################################################################
+
+SLURM--time=0-02:00:00
+SLURM--mem=64G
+SLURM--cpus-per-task=8
+SLURM--partition=cpu
+
+define slurm
+#! /usr/bin/bash
+#SBATCH --partition=$(SLURM--partition)
+#SBATCH --time='$(SLURM--time)'
+#SBATCH --cpus-per-task=$(SLURM--cpus-per-task)
+#SBATCH --mem=$(SLURM--mem)
+#SBATCH --job-name=$(notdir $(CURDIR))
+#SBATCH --output=slurm-%x-%A_%a.out
+endef
+export slurm
